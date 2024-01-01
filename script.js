28c7ddeb091f3e9fdad5246f59b96fe7e8b956ef
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-01-01 22:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+const evaluatedText = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
 
+evaluatedText.addEventListener('input', function(){
+	const text = evaluatedText.value;
+	letterCount.textContent = text.length;
+});
